ac7f28e010ab10af02e8f51307f58962e23f000d
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-03-29 14:18:55 +0800

    update model relationship

--- practical_03/VideoJotter/config/DBConnection.js
+++ practical_03/VideoJotter/config/DBConnection.js
@@ -1,6 +1,6 @@
 const mySQLDB = require('./DBConfig');
-const user = require('../models/User');
-const video = require('../models/Video');
+const User = require('../models/User');
+const Video = require('../models/Video');
 
 // If drop is true, all existing tables are dropped and recreated 
 const setUpDB = (drop) => {
@@ -11,7 +11,8 @@ const setUpDB = (drop) => {
             Defines the relationship where a user has many videos. 
             The primary key from user will be a foreign key in video. 
             */
-            user.hasMany(video);
+            User.hasMany(Video);
+            Video.belongsTo(User);
             mySQLDB.sync({
                 force: drop
             });
