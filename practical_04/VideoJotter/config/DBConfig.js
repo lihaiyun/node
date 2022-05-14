9b1174ab7c39ca2a6af528f590dbe1e88870c92e
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-05-14 10:37:24 +0800

    Use .env DB_PORT

--- practical_04/VideoJotter/config/DBConfig.js
+++ practical_04/VideoJotter/config/DBConfig.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD,
     {
         host: process.env.DB_HOST, // Name or IP of MySQL server
+        port: process.env.DB_PORT, // Port of MySQL server
         dialect: 'mysql', // Tells squelize that MySQL is used
         logging: false, // Disable logging; default: console.log
         pool: {
