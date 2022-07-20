78aafe4103a4197ef0d9a02fcbadbfdfac3342a7
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-20 14:41:16 +0800

    Use Bootstrap 5

--- practical_10/VideoJotter_BS5/public/js/common.js
+++ practical_10/VideoJotter_BS5/public/js/common.js
@@ -51,12 +51,6 @@ function getOMdbMovie() {
         })
 }
 
-// Display selected file name
-$(".custom-file-input").on("change", function () {
-    var fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
-
 // Use fetch to call post route /video/upload
 $('#posterUpload').on('change', function () {
     let formdata = new FormData();
